d946dd2aeac446f37d17f1b9df1e3a3c30de4a3e
Thomas <thommyho1988@gmail.com>
2022-07-03 21:40:09 +0200

    Update conanfile.py

--- test/conanfile.py
+++ test/conanfile.py
@@ -1,10 +1,8 @@
 from conans import ConanFile, CMake
 
 
-class ImguiOpencvDemo(ConanFile):
+class TestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "poco/1.10.1"
-
     generators = "cmake"
 
     def build(self):
@@ -13,9 +11,4 @@ class ImguiOpencvDemo(ConanFile):
         cmake.build()
 
     def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy("imgui_impl_glfw.cpp", dst="../src", src="./res/bindings")
-        self.copy("imgui_impl_opengl3.cpp", dst="../src", src="./res/bindings")
-        self.copy("imgui_impl_glfw.h*", dst="../include", src="./res/bindings")
-        self.copy("imgui_impl_opengl3.h*", dst="../include", src="./res/bindings")
+        pass
